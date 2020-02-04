ffcf1f074dfdbab402a6b325728c013786702a83
b <bgrant11@binghamton.edu>
2020-02-03 23:23:41 -0500

    about to try cluster

---
+++ polished/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=project1
+# Number of cores
+#SBATCH --cpus-per-task=24
+# Number of Nodes 
+#SBATCH -N 1
+# Assign memory usage in Megabytes 
+#SBATCH --mem=1024
+
+# Run for n trials for n threads
+A=-1
+B=1
+iterations=100000000 # 8 zeros
+n_threads=40
+trials=5
+
+for ((j=1;j<=$trials;j++));
+do
+   for ((i=1;i<=$n_threads;i++));
+   do 
+     (time ./integrate $A $B $iterations $i) &>> trial-$j.txt 
+  done
+done
